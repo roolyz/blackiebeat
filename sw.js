ee22491b926ff19d8e7fdf9c110ad317f9886786
roolyz <cecilajensen@gmail.com>
2026-09-02 06:18:11 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,7 @@
-var C='bs2';
 self.addEventListener('install',function(){self.skipWaiting();});
 self.addEventListener('activate',function(e){
  e.waitUntil(caches.keys().then(function(k){
   return Promise.all(k.map(function(n){return caches.delete(n);}));
  }).then(function(){return self.clients.claim();}));});
 self.addEventListener('fetch',function(e){
- e.respondWith(fetch(e.request).then(function(res){
-  var copy=res.clone();
-  caches.open(C).then(function(c){c.put(e.request,copy);});
-  return res;
- }).catch(function(){
-  return caches.match(e.request).then(function(r){return r||caches.match('./');});
- }));});
+ e.respondWith(fetch(e.request).catch(function(){return caches.match(e.request);}));});
