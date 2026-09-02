891ac2e7fe22409cdde90db21ba191cec8bf7eb5
roolyz <cecilajensen@gmail.com>
2026-09-02 13:12:28 +0700

    Delete fix.js

--- fix.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('touchstart',function(e){
- var b=e.target&&e.target.closest?e.target.closest('button'):null;
- if(window.state!=='play'||b)e.stopImmediatePropagation();
-},true);
-document.addEventListener('touchend',function(e){
- if(window.state!=='play')e.stopImmediatePropagation();
-},true);
